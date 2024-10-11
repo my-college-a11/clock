032fa4bac98e68148e16783a624744bf519d9e70
my-college-a11 <zahaiman2@gmail.com>
2024-10-11 09:59:16 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Clock from './component/Clock'
+
+function App() {
+  return (
+    <>
+    <div className='container'>
+      <Clock />
+    </div>
+    </>
+  )
+}
+
+export default App
